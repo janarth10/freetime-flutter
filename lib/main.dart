9490608b51b0e0176008436f47c8adab8699a14f
janarth10 <janarthp10@gmail.com>
2021-06-05 14:24:47 -0400

    moving data to represent and render spare time activity cards

--- lib/main.dart
+++ lib/main.dart
@@ -47,26 +47,59 @@ final fontFamilies = [
   'ZCOOLKuaiLe',
 ];
 
-String freeTimeChoice = activitiesList[0];
-String randomFontFamily = fontFamilies[0];
-
 Color hexToColor(String code) {
   return new Color(int.parse(code.substring(1, 7), radix: 16) + 0xFF000000);
 }
 
+class SpareTimeActivity {
+  const SpareTimeActivity({
+    @required this.activityName,
+    @required this.fontFamily,
+    // @required this.activityColorCode,
+    // @required this.iconName,
+  });
+
+  final String activityName;
+  final String fontFamily;
+  // final String activityColorCode;
+  // final String iconName;
+
+}
+
+// TODO: Won't need this until I implement the scrollable list view
+// List<SpareTimeActivity> getActivities() => activitiesList.map((x) => SpareTimeActivity(activityName: x)).toList();
+
+class SpareTimeCard extends StatelessWidget {
+  const SpareTimeCard({@required this.activity});
+  
+  final SpareTimeActivity activity;
+
+  Widget build(BuildContext context){
+    return Text(
+      activity.activityName,
+      style: TextStyle(
+        color: hexToColor("#f2a03d"), 
+        fontSize: 45.0,
+        fontFamily: activity.fontFamily
+      )
+    );
+  }
+}
+
 class _FreeTimeState extends State<FreeTime> {
+  String freeTimeChoice = activitiesList[0];
+  String randomFontFamily = fontFamilies[0];
+
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisSize: MainAxisSize.min,
       children: <Widget>[
-        Text(
-          freeTimeChoice,
-          style: TextStyle(
-            color: hexToColor("#f2a03d"), 
-            fontSize: 45.0,
-            fontFamily: randomFontFamily
-          )
+        SpareTimeCard(
+          activity: SpareTimeActivity(
+              activityName: freeTimeChoice,
+              fontFamily: randomFontFamily,
+            )
         ),
         Padding(padding: EdgeInsets.only(top: 100.0)),
         IconButton(
