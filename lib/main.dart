68adc80169f1aed9b2b6b9468e249baf843c7dc8
janarth10 <janarthp10@gmail.com>
2021-06-11 22:44:38 -0400

    initial-roulette-animation

--- lib/main.dart
+++ lib/main.dart
@@ -21,53 +21,60 @@ class MyApp extends StatelessWidget {
   }
 }
 
-
 class FreeTime extends StatefulWidget {
   @override
   _FreeTimeState createState() => _FreeTimeState();
 }
 
-final activitiesList = [
-  'Meditate', 
-  'Make Coffee', 
-  'Quuick Lap Round Vic Park',
-  'Pulse Breath Work',
-  'Stomach Massage Breath Work',
-  'Box Breath Work',
-];
-final fontFamilies = [
-  'CabinSketch',
-  'Playball',
-  'CinzelDecorative',
-  'LobsterTwo',
-  'Monoton',
-  'MountainsofChristmas',
-  'PressStart2p',
-  'YeonSung',
-  'ZCOOLKuaiLe',
-];
+class _FreeTimeState extends State<FreeTime> {
+  String freeTimeChoice = activitiesList[0];
 
-Color hexToColor(String code) {
-  return new Color(int.parse(code.substring(1, 7), radix: 16) + 0xFF000000);
+  final ScrollController controller = ScrollController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        Container(
+          height: 400,
+          child: ListView.builder(
+            controller: controller,
+            scrollDirection: Axis.horizontal,
+            itemCount: activitiesList.length,
+            itemBuilder: (BuildContext context, int index) {
+              return SpareTimeCard(
+                activity: SpareTimeActivity(
+                    name: activitiesList[index],
+                    fontFamily: 'CabinSketch',
+                  )
+              );
+            }
+          )
+        ),
+        Padding(padding: EdgeInsets.only(top: 100.0)),
+        IconButton(
+          icon: const Icon(Icons.offline_bolt_rounded),
+          iconSize: 100,
+          tooltip: "Freetime Flutter Lottery!",
+          onPressed: () {
+            setState(() {
+              Random rnd = new Random();
+              int randInt = rnd.nextInt(activitiesList.length);
+
+              double scrollToOffset = calculateWidgetScrollOffset(randInt, controller.position.maxScrollExtent);
+              controller.animateTo(scrollToOffset, duration: Duration(seconds: 1), curve: Curves.easeIn);
+            });
+          },
+        ),
+      ],
+    );
+  }
 }
 
-class SpareTimeActivity {
-  const SpareTimeActivity({
-    @required this.name,
-    @required this.fontFamily,
-    this.colorCode = "#f2a03d",
-    // @required this.iconName,
-  });
-
-  final String name;
-  final String fontFamily;
-  final String colorCode;
-  // final String iconName;
-
-}
-
-// TODO: Won't need this until I implement the scrollable list view
-// List<SpareTimeActivity> getActivities() => activitiesList.map((x) => SpareTimeActivity(name: x)).toList();
+// 
+//  Helper Classes
+// 
 
 class SpareTimeCard extends StatelessWidget {
   const SpareTimeCard({@required this.activity});
@@ -99,39 +106,67 @@ class SpareTimeCard extends StatelessWidget {
   }
 }
 
-class _FreeTimeState extends State<FreeTime> {
-  String freeTimeChoice = activitiesList[0];
-  String randomFontFamily = fontFamilies[0];
+class SpareTimeActivity {
+  const SpareTimeActivity({
+    @required this.name,
+    @required this.fontFamily,
+    this.colorCode = "#f2a03d",
+    // @required this.iconName,
+  });
 
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: <Widget>[
-        SpareTimeCard(
-          activity: SpareTimeActivity(
-              name: freeTimeChoice,
-              fontFamily: randomFontFamily,
-            )
-        ),
-        Padding(padding: EdgeInsets.only(top: 100.0)),
-        IconButton(
-          icon: const Icon(Icons.offline_bolt_rounded),
-          iconSize: 100,
-          tooltip: "Freetime Flutter Lottery!",
-          onPressed: () {
-            setState(() {
-              Random rnd = new Random();
+  final String name;
+  final String fontFamily;
+  final String colorCode;
+  // final String iconName;
 
-              freeTimeChoice = activitiesList[rnd.nextInt(activitiesList.length)];
-              randomFontFamily = fontFamilies[rnd.nextInt(fontFamilies.length)];
-
-              print(freeTimeChoice);
-              print(randomFontFamily);
-            });
-          },
-        ),
-      ],
-    );
-  }
 }
+
+// 
+//  Helper Functions
+// 
+
+Color hexToColor(String code) {
+  return new Color(int.parse(code.substring(1, 7), radix: 16) + 0xFF000000);
+}
+
+double calculateWidgetScrollOffset(int widgetIndex, double maxScrollOffset){
+  return widgetIndex * maxScrollOffset / (activitiesList.length - 1);
+}
+
+// 
+//  Constants 
+// 
+
+final activitiesList = [
+  // Zen
+  'Clear Mind Meditation',
+  'Day Dream Meditation' 
+  'Pulse Breath Work',
+  'Stomach Massage Breath Work',
+  'Box Breath Work',
+  // Mind
+  'Read, oh wise one',
+  'Audible at home',
+  'Brilliant.org',
+  'Brain dump writing',
+  'Write a post',
+  // The Great Outdoors
+  "Speedy lap round Vic' park",
+  "Coffee/Snack Drive",
+  // Chore or Challenge
+  'Vaccuum room and living room',
+  'Laundry',
+  'Cold shower',
+];
+
+final fontFamilies = [
+  'CabinSketch',
+  'Playball',
+  'CinzelDecorative',
+  'LobsterTwo',
+  'Monoton',
+  'MountainsofChristmas',
+  'PressStart2p',
+  'YeonSung',
+  'ZCOOLKuaiLe',
+];
